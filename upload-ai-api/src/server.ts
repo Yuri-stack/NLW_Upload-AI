f01de094e3d054b2c07c9301d8bc3e68bf022692
Yuri-stack <yurioliveirasilva1999@gmail.com>
2023-09-16 21:54:12 -0300

    Feat.: Finalizando o Desenvolvimento do Back

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,20 @@
 import { fastify } from 'fastify'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { fastifyCors } from '@fastify/cors'
+
 import { uploadVideoRoute } from './routes/upload-video'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { createTranscriptionRoute } from './routes/create-transcriptions'
+import { generateAiCompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
 
+app.register(fastifyCors, {
+    origin: '*'
+})
+
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({ port: 3333 }).then(() => { console.log("Its running") })
